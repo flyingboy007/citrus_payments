c350fa4adfde9eacaa2a7e4a54d4b9a4183fa0f7
flyingboy007 <abhilashvr007@gmail.com>
2017-07-17 11:01:47 +0530

    added seller update for splitpay

--- lib/citrus_payments/marketplace/seller.rb
+++ lib/citrus_payments/marketplace/seller.rb
@@ -29,6 +29,31 @@ module CitrusPayments
         end
       end
 
+      def self.update(merchant_auth_token, seller_attributes)
+        uri = URI.parse('https://splitpaysbox.citruspay.com/marketplace/seller/')
+        request = Net::HTTP::Put.new(uri)
+        request.content_type = 'application/json'
+        request['Auth_token']= merchant_auth_token
+        request.body = JSON.dump(seller_attributes)
+        req_options = {
+            use_ssl: uri.scheme == 'https',
+        }
+
+        response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+          http.request(request)
+        end
+        parsed_response=JSON.parse(response.body, :symbolize_names => true)
+
+        #If input errors response will be in array in weird format
+        if parsed_response.is_a?(Array)
+          parsed_response.first[:property].slice!("instance.") #removes instance. string from response
+          #making a response similar to other errors
+          altered_response={error_id: "7", error_category: "stack", error_description: parsed_response.first[:property]+" "+parsed_response.first[:message]}
+        else
+          parsed_response
+        end
+      end
+
     end
   end
 end
